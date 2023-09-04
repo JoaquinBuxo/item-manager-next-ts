3eccf1535509daf74fb531b909d5443a847b4500
Joaquín Buxó <joaquin.buxo@gmail.com>
2023-09-04 21:10:23 +0200

    feat: fetch data in the server

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,23 @@
-export default function Home() {
+import { Product } from '@/types';
+
+const fetchProducts = () => {
+  return fetch(
+    'https://frontend-tech-test-data.s3-eu-west-1.amazonaws.com/items.json'
+  ).then((data) => data.json());
+};
+
+export default async function Home() {
+  const products = await fetchProducts();
+
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'></main>
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <section>
+        {products.items.map((product: Product, index: number) => (
+          <article key={index}>
+            <h2>{product.title}</h2>
+          </article>
+        ))}
+      </section>
+    </main>
   );
 }
---
+++ types/index.ts
@@ -0,0 +1,8 @@
+export interface Product {
+  title: string;
+  description: string;
+  price: string;
+  url: string;
+  email: string;
+  image: string;
+}
