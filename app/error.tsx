086f31e138239c5090f3623630ae821e1abb6458
Joaquín Buxó <joaquin.buxo@gmail.com>
2023-09-06 17:04:10 +0200

    feat: create error file to show errors

---
+++ app/error.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react';
+
+const Error = () => {
+  return (
+    <div className='flex min-h-screen flex-col items-center justify-between p-24'>
+      Error loading Products
+    </div>
+  );
+};
+
+export default Error;
