2b647fb388b8ec7f46637ead39f18a65dd9d7e03
Joaquín Buxó <joaquin.buxo@gmail.com>
2023-09-08 13:00:38 +0200

    feat: add spinner in loading page

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Loading = () => {
   return (
     <div className='flex min-h-screen flex-col items-center justify-between p-24'>
-      LOADING PRODUCTS...
+      <div className='fixed top-0 right-0 h-screen w-screen z-50 flex justify-center items-center'>
+        <div className='animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900'></div>
+      </div>
     </div>
   );
 };
