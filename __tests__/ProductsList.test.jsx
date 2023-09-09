2ef7b79ab527ef8c4200e4f0228fddafdf36ac58
Joaquín Buxó <joaquin.buxo@gmail.com>
2023-09-09 13:22:35 +0200

    test: create test for products list

---
+++ __tests__/ProductsList.test.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { useProducts } from '../hooks/useProducts';
+import ProductsList from '../components/ProductsList';
+import mockProducts from '../mocks/products';
+
+jest.mock('../hooks/useProducts');
+
+describe('ProductsList', () => {
+  beforeEach(() => {
+    useProducts.mockReturnValue({
+      paginatedProducts: mockProducts.items,
+      isProductFavorite: jest.fn(),
+    });
+  });
+
+  it('renders correctly', () => {
+    render(<ProductsList />);
+    mockProducts.items.forEach((product) => {
+      expect(screen.getByText(product.title)).toBeInTheDocument();
+    });
+  });
+
+  it('renders "No products" message when there are no products', () => {
+    useProducts.mockReturnValueOnce({
+      paginatedProducts: [],
+    });
+    render(<ProductsList />);
+    expect(screen.getByText('No products')).toBeInTheDocument();
+  });
+});
