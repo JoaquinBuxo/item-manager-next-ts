f1c1255ce3ea4b3c54ca2c3729b446795ac890ab
Joaquín Buxó <joaquin.buxo@gmail.com>
2023-09-09 13:05:15 +0200

    feat: create product card tests

---
+++ __tests__/ProductCard.test.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { useProducts } from '../hooks/useProducts';
+import ProductCard from '../components/ProductCard';
+import mockProducts from '../mocks/products';
+
+jest.mock('../hooks/useProducts');
+
+describe('ProductCard', () => {
+  const mockProduct = mockProducts.items[0];
+  beforeEach(() => {
+    useProducts.mockReturnValue({
+      isProductFavorite: jest.fn(),
+    });
+  });
+
+  it('renders correctly', () => {
+    render(<ProductCard product={mockProduct} />);
+    expect(screen.getByText(mockProduct.title)).toBeInTheDocument();
+    expect(screen.getByText(mockProduct.description)).toBeInTheDocument();
+    expect(screen.getByText(mockProduct.price + ' EUR')).toBeInTheDocument();
+    expect(screen.getByText(mockProduct.email)).toBeInTheDocument();
+    expect(screen.getByAltText(mockProduct.title)).toBeInTheDocument();
+  });
+});
