f3a09bdbf4fe833ffc3246e34074a0bc13cc76eb
Joaquín Buxó <joaquin.buxo@gmail.com>
2023-09-09 13:41:11 +0200

    test: create search tests

---
+++ __tests__/Search.test.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Search from '../components/Search';
+
+describe('Search', () => {
+  it('calls setSearchQuery with input value on change', () => {
+    const setSearchQuery = jest.fn();
+    const { getByPlaceholderText } = render(
+      <Search setSearchQuery={setSearchQuery} />
+    );
+    const input = getByPlaceholderText('Search Product');
+
+    fireEvent.change(input, { target: { value: 'test' } });
+
+    expect(setSearchQuery).toHaveBeenCalledWith('test');
+  });
+
+  it('does not call setSearchQuery when input value is empty', () => {
+    const setSearchQuery = jest.fn();
+    const { getByPlaceholderText } = render(
+      <Search setSearchQuery={setSearchQuery} />
+    );
+    const input = getByPlaceholderText('Search Product');
+
+    fireEvent.change(input, { target: { value: '' } });
+
+    expect(setSearchQuery).not.toHaveBeenCalled();
+  });
+});
