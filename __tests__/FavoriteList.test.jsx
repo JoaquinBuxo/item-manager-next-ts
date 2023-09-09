7a8dfa7338d055ccb1d1bcdd4548df619d5d6653
Joaquín Buxó <joaquin.buxo@gmail.com>
2023-09-09 11:26:52 +0200

    test: create test for favorite list

---
+++ __tests__/FavoriteList.test.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import FavoriteList from '../components/FavoriteList';
+import { useProducts } from '@/hooks/useProducts';
+import mockProducts from '@/mocks/products';
+
+jest.mock('../hooks/useProducts');
+
+describe('FavoriteList', () => {
+  const mockToggleOpenFavoriteList = jest.fn();
+  const mockSetSearchFavoriteQuery = jest.fn();
+
+  beforeEach(() => {
+    useProducts.mockReturnValue({
+      filteredFavoriteProducts: mockProducts.items,
+      toggleOpenFavoriteList: mockToggleOpenFavoriteList,
+      setSearchFavoriteQuery: mockSetSearchFavoriteQuery,
+    });
+  });
+
+  it('renders correctly', () => {
+    render(<FavoriteList open={true} />);
+
+    expect(screen.getByText('Favorite Products')).toBeInTheDocument();
+    expect(screen.getByText(mockProducts.items[0].title)).toBeInTheDocument();
+  });
+});
