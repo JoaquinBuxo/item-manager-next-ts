7b471197a1ec02e1000a8971367815e1b70b267e
Joaquín Buxó <joaquin.buxo@gmail.com>
2023-09-09 11:38:21 +0200

    test: create test for favorite list button

---
+++ __tests__/FavoriteListButton.test.jsx
@@ -0,0 +1,28 @@
+import { render, screen } from '@testing-library/react';
+import FavoriteListButton from '../components/FavoriteListButton';
+import { useProducts } from '../hooks/useProducts';
+
+jest.mock('../hooks/useProducts');
+
+describe('FavoriteListButton', () => {
+  const mockToggleOpenFavoriteList = jest.fn();
+
+  beforeEach(() => {
+    useProducts.mockReturnValue({
+      toggleOpenFavoriteList: mockToggleOpenFavoriteList,
+    });
+  });
+
+  it('renders correctly', () => {
+    render(<FavoriteListButton />);
+
+    expect(screen.getByRole('button')).toBeInTheDocument();
+  });
+
+  it('calls toggleOpenFavoriteList when clicked', () => {
+    render(<FavoriteListButton />);
+    screen.getByRole('button').click();
+
+    expect(mockToggleOpenFavoriteList).toHaveBeenCalled();
+  });
+});
