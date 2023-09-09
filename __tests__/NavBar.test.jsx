d89763e429a727d16f6f682148a35b82979b108b
Joaquín Buxó <joaquin.buxo@gmail.com>
2023-09-09 11:47:11 +0200

    test: create navbar test

---
+++ __tests__/NavBar.test.jsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import NavBar from '../components/NavBar';
+
+describe('NavBar', () => {
+  it('renders correctly', () => {
+    render(<NavBar />);
+    const navBar = screen.getByRole('navigation');
+    expect(navBar).toBeInTheDocument();
+    expect(screen.getByText('CATALOGUE')).toBeInTheDocument();
+  });
+
+  it('renders FavoriteListButton', () => {
+    render(<NavBar />);
+    const favoriteListButton = screen.getByRole('button');
+    expect(favoriteListButton).toBeInTheDocument();
+  });
+});
